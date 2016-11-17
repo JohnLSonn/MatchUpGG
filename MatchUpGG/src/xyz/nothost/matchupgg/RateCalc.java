a0695be0de91aeb43f5ceb310daf2555762e3e3f
G015C1301 <G015C1301@G15C1301-PC>
2016-11-17 19:53:00 +0900

    ＲａｔｅＣａｌｃ送るの忘れた

---
+++ MatchUpGG/src/xyz/nothost/matchupgg/RateCalc.java
@@ -0,0 +1,8 @@
+package xyz.nothost.matchupgg;
+
+public class RateCalc {
+	WinRate = WinCount/GameCount*100;
+	MateWinRate = MateWinCount/MateGameCount*100;
+	VsWinRate = VsWinCount/VsGameCount*100;
+
+}
